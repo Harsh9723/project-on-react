80ea899c64bb18dbc6361a827839de306f024b96
Harsh sheladiya <shv245245@gmail.com>
2024-02-03 15:21:47 +0530

    react

---
+++ counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let counter =15
+  const addValue = () => {
+    counter = counter + 1
+   
+  }
+
+  return(
+    <>
+    <h1>chai aur code</h1>
+    <h2>counter value: {counter}</h2>
+    <button onClick={addValue}>add  value</button><br />
+    <button>remove value</button>
+   
+    </>
+  )
+   
+}
+
+
+export default App
